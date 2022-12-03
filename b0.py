f8ce2b36c2f6565280d46b5170f7e7b8bffb6482
RafaelLeone <rinaciodacosta@gmail.com>
2022-12-03 15:35:35 -0300

    dia 3 metade + melhorias

--- b0.py
+++ b0.py
@@ -1,13 +1,14 @@
 with open("./b-dados") as f:
     dados = f.readlines()
 pontuacao = 0
+# Fórmula do sistema: pontuação pela forma escolhida + pontuação por derrota, empate ou vitória.
 sistema = {
-    'B X': 1, 'C Y': 2,'A Z': 3, 
-    'A X': 4, 'B Y': 5, 'C Z': 6, 
-    'C X': 7, 'A Y': 8, 'B Z': 9,
-    'B X\n': 1, 'C Y\n': 2,'A Z\n': 3, 
-    'A X\n': 4, 'B Y\n': 5, 'C Z\n': 6, 
-    'C X\n': 7, 'A Y\n': 8, 'B Z\n': 9
+    'B X': 1+0, 'C Y': 2+0, 'A Z': 3+0, 
+    'A X': 1+3, 'B Y': 2+3, 'C Z': 3+3, 
+    'C X': 1+6, 'A Y': 2+6, 'B Z': 3+6,
+    'B X\n': 1+0, 'C Y\n': 2+0, 'A Z\n': 3+0, 
+    'A X\n': 1+3, 'B Y\n': 2+3, 'C Z\n': 3+3, 
+    'C X\n': 1+6, 'A Y\n': 2+6, 'B Z\n': 3+6
     }
 
 for rodada in dados:
--- b1.py
+++ b1.py
@@ -1,13 +1,14 @@
 with open("./b-dados") as f:
     dados = f.readlines()
 pontuacao = 0
+# Fórmula do sistema: pontuação pela forma escolhida + pontuação por derrota, empate ou vitória.
 sistema = {
-    'B X': 1, 'C Y': 6,'A Z': 8, 
-    'A X': 3, 'B Y': 5, 'C Z': 7, 
-    'C X': 2, 'A Y': 4, 'B Z': 9,
-    'B X\n': 1, 'C Y\n': 6,'A Z\n': 8, 
-    'A X\n': 3, 'B Y\n': 5, 'C Z\n': 7, 
-    'C X\n': 2, 'A Y\n': 4, 'B Z\n': 9
+    'B X': 1+0, 'C Y': 3+3, 'A Z': 2+6, 
+    'A X': 3+0, 'B Y': 2+3, 'C Z': 1+6, 
+    'C X': 2+0, 'A Y': 1+3, 'B Z': 3+6,
+    'B X\n': 1+0, 'C Y\n': 3+3, 'A Z\n': 2+6, 
+    'A X\n': 3+0, 'B Y\n': 2+3, 'C Z\n': 1+6, 
+    'C X\n': 2+0, 'A Y\n': 1+3, 'B Z\n': 3+6
     }
 
 for rodada in dados:
