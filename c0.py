a2b11c49e8fa9dfdf99c90f99ab4a147b2f7ea55
RafaelLeone <rinaciodacosta@gmail.com>
2022-12-03 15:29:05 -0300

    dia 3 metade

--- c0.py
+++ c0.py
@@ -1,5 +1,10 @@
 with open("./c-dados") as f:
     dados = [l.strip('\n\r') for l in f]
+compartimento1 = []
+compartimento2 = []
+contador = 0
+soma = 0
+# letras_que_ja_foram = []
 #Dicionário de prioridade:
 dict = {}
 for char in range(26):
@@ -8,10 +13,20 @@ for char in range(26):
 for char in range(26):
     c = char + 65
     dict[char+27] = chr(c)
+dict = {value:key for key, value in dict.items()}
 #Dividindo compartimentos:
 for mochila in dados:
     divisao = int(len(mochila)/2)
-    compartimento1 = mochila[:divisao]
-    compartimento2 = mochila[divisao:]
+    compartimento1.append(mochila[:divisao])
+    compartimento2.append(mochila[divisao:])
 
-print(divisao, compartimento1, compartimento2)
+while contador < len(compartimento1):
+    for letra in compartimento1[contador]:
+        if letra in compartimento2[contador]:
+            # and letra not in letras_que_ja_foram
+            # letras_que_ja_foram.append(letra)
+            soma += dict[letra]
+            break
+    contador += 1
+
+print(soma)
