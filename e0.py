f963b1b8ea289c97801a797be008bf825116c629
RafaelLeone <rinaciodacosta@gmail.com>
2022-12-05 11:31:20 -0300

    lista de movimentos

--- e0.py
+++ e0.py
@@ -1,3 +1,5 @@
+import re
+
 with open("./e-dados") as f:
     dados = [l.strip('\n\r') for l in f]
 linha = 0
@@ -5,7 +7,6 @@ numero_a_pular = 4
 tabela = []
 matriz = []
 contador = 0
-numero_atual = 0
 #Matriz de caixas:
 while linha < len(dados):
     if dados[linha][1] == '1':
@@ -15,14 +16,23 @@ while linha < len(dados):
         if (index-1)%4 == 0:
             tabela.append(letra)
     linha += 1
-while numero_atual < numero_da_ultima_pilha:
+linha = 0
+while linha < numero_da_ultima_pilha:
     matriz.append([])
     while contador < len(tabela):
         if tabela[contador] != ' ':
-            matriz[numero_atual].append(tabela[contador])
+            matriz[linha].append(tabela[contador])
         contador += numero_da_ultima_pilha
-    matriz[numero_atual].reverse()
-    numero_atual += 1
-    contador = numero_da_ultima_pilha - (numero_da_ultima_pilha - numero_atual)
+    matriz[linha].reverse()
+    linha += 1
+    contador = numero_da_ultima_pilha - (numero_da_ultima_pilha - linha)
+#Listas de movimentos:
+linha +=1
+lista_de_movimentos = []
+print(linha)
+while linha < len(dados):
+    lista_de_movimentos.append([])
+    lista_de_movimentos[linha-10] = [int(s) for s in re.findall(r'\d+', dados[linha])]
+    linha += 1
 
-print(matriz)
+print(lista_de_movimentos)
