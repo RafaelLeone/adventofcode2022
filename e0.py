bce559f244d7c3549ddc1fa9ff066cc1b946c80a
RafaelLeone <rinaciodacosta@gmail.com>
2022-12-05 11:01:36 -0300

    precisa inverter listas da lista eu acho

--- e0.py
+++ e0.py
@@ -3,16 +3,25 @@ with open("./e-dados") as f:
 linha = 0
 numero_a_pular = 4
 tabela = []
+matriz = []
+contador = 0
+numero_atual = 0
 #Tabela de caixas:
 while linha < len(dados):
     if dados[linha][1] == '1':
-        numero_da_ultima_pilha = dados[linha][-2]
+        numero_da_ultima_pilha = int(dados[linha][-2])
         break
     for index, letra in enumerate(dados[linha]):
-        if letra == chr(32):
-            print('ok', index, linha)
         if (index-1)%4 == 0:
             tabela.append(letra)
     linha += 1
+while numero_atual < numero_da_ultima_pilha:
+    matriz.append([])
+    while contador < len(tabela):
+        if tabela[contador] != ' ':
+            matriz[numero_atual].append(tabela[contador])
+        contador += numero_da_ultima_pilha
+    numero_atual += 1
+    contador = numero_da_ultima_pilha - (numero_da_ultima_pilha - numero_atual)
 
-print(tabela)
+print(matriz)
